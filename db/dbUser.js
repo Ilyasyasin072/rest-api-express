7857989607f7b8320cfa3c3bc38f68bc5b26664d
Ilyas Yasin <ilyas@mewahniagajaya.com>
2021-03-11 13:31:17 +0700

    add model, show, update, delete

--- db/dbUser.js
+++ db/dbUser.js
@@ -41,7 +41,55 @@ const postUserModel = (
 }
 
 
+const updateUserModel = (nama_depan,
+    nama_belakang,
+    jenis_kelamin,
+    tanggal_lahir,
+    email,
+    password,
+    aktifasi,
+    alamat,
+    no_tlp,
+    phone,
+    id_provinsi,
+    id_kota) => {
+
+    var data = {
+        nama_depan: nama_depan,
+        nama_belakang: nama_belakang,
+        jenis_kelamin: jenis_kelamin,
+        tanggal_lahir: tanggal_lahir,
+        email: email,
+        password: password,
+        aktifasi: aktifasi,
+        alamat: alamat,
+        no_tlp: no_tlp,
+        phone: phone,
+        id_provinsi: id_provinsi,
+        id_kota: id_kota,
+    }
+
+    var getId = parseInt(id);
+
+    return User.query().findById(getId).patch(data);
+}
+
+const showUserModel = (id) => {
+    var getId = parseInt(id);
+    return User.query().findById(getId);
+}
+
+const deleteUserModel = (id) => {
+    var getId = parseInt(id);
+
+    return User.query().deleteById(getId);
+}
+
+
 module.exports = {
     gerUserModel,
-    postUserModel
+    postUserModel,
+    updateUserModel,
+    showUserModel,
+    deleteUserModel
 }
